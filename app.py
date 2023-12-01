9782be4189d57c05f93baf14342fb640c500a48a
Jonathan Hofmann <122258749+hofmannj0n@users.noreply.github.com>
2023-12-01 15:54:12 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+
+import streamlit as st
+import os
+
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
+
+API_KEY = "79ab295c19394593829bfd22215cff98"
+ENDPOINT = "https://feather-form-recognizer.cognitiveservices.azure.com/"
+
+def extract_text_from_document(document_path):
+    document_analysis = DocumentAnalysisClient(endpoint=ENDPOINT, credential=AzureKeyCredential(API_KEY))
+
+    with open(document_path, 'rb') as f:
+        poller = document_analysis.begin_analyze_document("prebuilt-document", f.read())
+        result = poller.result()
+
+        extracted_text = " "
+
+        for page in result.pages:
+            for line in page.lines:
+                extracted_text += line.content + " "
+
+        return extracted_text.strip()
+    
+st.header('PDF Text Extraction', divider='rainbow')
+
+uploaded_file = st.file_uploader("Choose a PDF file...", type=["pdf"])
+
+if uploaded_file is not None:
+    
+    # Display the uploaded PDF file
+    st.write("Uploaded PDF file:", uploaded_file.name)
+
+    # Perform OCR using Azure Form Recognizer
+    st.write("Running OCR...")
+
+    # Save the uploaded file temporarily
+    temp_file_path = "/tmp/uploaded_pdf.pdf"
+    with open(temp_file_path, 'wb') as temp_file:
+        temp_file.write(uploaded_file.read())
+
+    # Extract text 
+    extracted_text = extract_text_from_document(temp_file_path)
+
+    # Remove the temporary file
+    os.remove(temp_file_path)
+
+    st.text_area("Extracted Text From Document:", extracted_text)
